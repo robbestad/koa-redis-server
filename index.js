5750b034da61775411a0425f6df063651e3b8be5
Sven Anders Robbestad <sven@robbestad.com>
2017-03-03 21:51:39 +0100

    Added security

--- index.js
+++ index.js
@@ -6,20 +6,33 @@ const bodyParser = require('koa-better-body');
 const favicon = require('koa-favicon');
 const convert = require('koa-convert');
 const parse = require('co-body');
+const crypto = require('crypto');
 const app = new koa();
 const Redis = require('koa-simple-redis');
 const client = new Redis({
   url: 'redis://127.0.0.1:6379',
 });
 const PORT = 8666;
+
 require('colors');
 
 const logger = require('debug');
 
 const redisKey = 'content';
 
+
+const validateHash = async(ctx, next) => {
+  const {hash, phrase} = ctx.req.headers;
+  const controlHash = crypto.createHash('sha256').update(require('./secret.json') + phrase).digest('base64');
+  if (hash !== controlHash) {
+    return ctx.throw(404);
+  }
+  await next();
+};
+
 router
   .get('/content',
+    validateHash,
     async ctx => {
       ctx.body = await new Promise((resolve, reject) => {
         client.get(redisKey)
@@ -32,23 +45,25 @@ router
       })
     }
   )
-  .put('/content', (async(ctx, next) => {
-    await new Promise((resolve, reject) => {
-      const json = parse.json(ctx).then(json => {
-        client.set(redisKey,
-          {"data": JSON.stringify(json)}, 10e10)
-          .then(res => {
-            ctx.body = res;
-            ctx.status = 201;
-            resolve();
-          })
-          .catch(err => {
-            ctx.status = 500;
-            reject();
-          })
-      })
-    });
-  }));
+  .put('/content',
+    validateHash,
+    (async(ctx, next) => {
+      await new Promise((resolve, reject) => {
+        const json = parse.json(ctx).then(json => {
+          client.set(redisKey,
+            {"data": JSON.stringify(json)}, 10e10)
+            .then(res => {
+              ctx.body = res;
+              ctx.status = 201;
+              resolve();
+            })
+            .catch(err => {
+              ctx.status = 500;
+              reject();
+            })
+        })
+      });
+    }));
 
 app
   .use(favicon(path.join(__dirname, 'favicon.ico')))
